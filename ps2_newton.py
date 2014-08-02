9667b2e3bd8649a23caaecc13cc0894bdaab0f26
D-Hizzy <dsg14@zips.uakron.edu>
2014-08-01 21:11:12 -0500

    Newton's method root finder done

--- ps2_newton.py
+++ ps2_newton.py
@@ -4,56 +4,65 @@
 #
 
 def evaluate_poly(poly, x):
-    """
-    Computes the polynomial function for a given value x. Returns that value.
+	"""
+	Computes the polynomial function for a given value x. Returns that value.
 
-    Example:
-    >>> poly = (0.0, 0.0, 5.0, 9.3, 7.0)    # f(x) = 7x^4 + 9.3x^3 + 5x^2
-    >>> x = -13
-    >>> print evaluate_poly(poly, x)  # f(-13) = 7(-13)^4 + 9.3(-13)^3 + 5(-13)^2
-    180339.9
+	Example:
+	>>> poly = (0.0, 0.0, 5.0, 9.3, 7.0)    # f(x) = 7x^4 + 9.3x^3 + 5x^2
+	>>> x = -13
+	>>> print evaluate_poly(poly, x)  # f(-13) = 7(-13)^4 + 9.3(-13)^3 + 5(-13)^2
+	180339.9
 
-    poly: tuple of numbers, length > 0
-    x: number
-    returns: float
-    """
-    # TO DO ... 
-
+	poly: tuple of numbers, length > 0
+	x: number
+	returns: float
+	"""
+	answer = sum([poly[i] * (x ** i) for i in range(len(poly))])
+	return answer
 
 def compute_deriv(poly):
-    """
-    Computes and returns the derivative of a polynomial function. If the
-    derivative is 0, returns (0.0,).
-
-    Example:
-    >>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    # x^4 + 3x^3 + 17.5x^2 - 13.39
-    >>> print compute_deriv(poly)        # 4x^3 + 9x^2 + 35^x
-    (0.0, 35.0, 9.0, 4.0)
-
-    poly: tuple of numbers, length > 0
-    returns: tuple of numbers
-    """
-    # TO DO ... 
-
+	"""
+	Computes and returns the derivative of a polynomial function. If the
+	derivative is 0, returns (0.0,).
+	Example:
+	>>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    # x^4 + 3x^3 + 17.5x^2 - 13.39
+	>>> print compute_deriv(poly)        # 4x^3 + 9x^2 + 35^x
+	(0.0, 35.0, 9.0, 4.0)
+	poly: tuple of numbers, length > 0
+	returns: tuple of numbers
+	"""
+	deriv = tuple([poly[i] * i for i in range(1,len(poly))])
+	return deriv
+		
 def compute_root(poly, x_0, epsilon):
-    """
-    Uses Newton's method to find and return a root of a polynomial function.
-    Returns a tuple containing the root and the number of iterations required
-    to get to the root.
+	"""
+	Uses Newton's method to find and return a root of a polynomial function.
+	Returns a tuple containing the root and the number of iterations required
+	to get to the root.
 
-    Example:
-    >>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    #x^4 + 3x^3 + 17.5x^2 - 13.39
-    >>> x_0 = 0.1
-    >>> epsilon = .0001
-    >>> print compute_root(poly, x_0, epsilon)
-    (0.80679075379635201, 8.0)
+	Example:
+	>>> poly = (-13.39, 0.0, 17.5, 3.0, 1.0)    #x^4 + 3x^3 + 17.5x^2 - 13.39
+	>>> x_0 = 0.1
+	>>> epsilon = .0001
+	>>> print compute_root(poly, x_0, epsilon)
+	(0.80679075379635201, 8.0)
 
-    poly: tuple of numbers, length > 1.
-         Represents a polynomial function containing at least one real root.
-         The derivative of this polynomial function at x_0 is not 0.
-    x_0: float
-    epsilon: float > 0
-    returns: tuple (float, int)
-    """
-    # TO DO ... 
+	poly: tuple of numbers, length > 1.
+		 Represents a polynomial function containing at least one real root.
+		 The derivative of this polynomial function at x_0 is not 0.
+	x_0: float
+	epsilon: float > 0
+	returns: tuple (float, int)
+	"""
+	numIters = 0
+	x_n = x_0
+	deriv = compute_deriv(poly)
+	while abs(evaluate_poly(poly, x_n)) > epsilon:
+		numIters += 1
+		x_n -= (evaluate_poly(poly, x_n)/evaluate_poly(deriv, x_n))
+	
+	return (x_n, numIters)
+
+print(compute_root((8,6,1),1,0.1))
+
 
